39f65fc49bf625ac37fe994da91b2c8eb8ed908c
JasonJanePY <49685719+JasonJanePY@users.noreply.github.com>
2019-05-10 15:02:49 +0800

    用户管理的contrller、service、mapper的更新

--- src/main/java/top/mnilsy/cup/contrller/UserManageContrller.java
+++ src/main/java/top/mnilsy/cup/contrller/UserManageContrller.java
@@ -14,6 +14,7 @@ import top.mnilsy.cup.utils.RequestMessage;
 import top.mnilsy.cup.utils.ResponMessage;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
